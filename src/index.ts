4994f8de46bacd8ace0728ad47ae763b59605ff7
ehgks0000 <ehgks0083@gmail.com>
2022-01-29 10:44:36 +0900

    Config: tsconfig update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 
-import express, { Request, Response, NextFunction } from 'express'
-import config from './config';
+import express, { Request, Response, NextFunction } from "express";
+import config from "./config";
 import colors from "colors";
 import routes from "./router";
 import { Mongo, logger, ExError, loggingReq } from "./utils";
@@ -9,34 +9,34 @@ import { Mongo, logger, ExError, loggingReq } from "./utils";
 const app: express.Application = express();
 
 async function start() {
-    app.use(express.json())
-    await Mongo.connect();
+  app.use(express.json());
+  await Mongo.connect();
 
-    app.use(loggingReq);
+  app.use(loggingReq);
 
-    app.use(config.api.prefix, routes)
-    
-    app.use(() => {
-        throw new ExError(404, 'Not Found');
-    });
+  app.use(config.api.prefix, routes);
 
-    /* eslint-disable */
-    app.use((err: ExError, req: Request, res: Response, next: NextFunction) => {
-        logger.error(colors.red(JSON.stringify(err)));
-    
-        const status = err.status? err.status : 500;
-        const message = err.message ? err.message : "SERVER_ERROR"
-    
-        res.status(status).json({ message });
-    });
-    
-    app.listen(3000, () => {
-        logger.info(`
+  app.use(() => {
+    throw new ExError(404, "Not Found");
+  });
+
+  /* eslint-disable */
+  app.use((err: ExError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(colors.red(JSON.stringify(err)));
+
+    const status = err.status ? err.status : 500;
+    const message = err.message ? err.message : "SERVER_ERROR";
+
+    res.status(status).json({ message });
+  });
+
+  app.listen(3000, () => {
+    logger.info(`
         ################################################
         🛡️  Server listening on port: ${config.port} 🛡️
         ################################################
         `);
-    })
+  });
 }
 
 start();
--- src/router/v1/user.router.ts
+++ src/router/v1/user.router.ts
@@ -1,17 +1,15 @@
 import express from "express";
 import { Container } from "typedi";
 import { IUserController } from "../../types/user";
-import { UserController } from "../../controllers"
-import { asyncHandler } from "../../utils"
+import { UserController } from "../../controllers";
+import { asyncHandler } from "../../utils";
 
 const router = express.Router();
 
-const userControllerInstance: IUserController = Container.get(UserController)
+const userControllerInstance: IUserController = Container.get(UserController);
 
-router.route("/")
-    .post(asyncHandler(userControllerInstance.create))
+router.route("/").post(asyncHandler(userControllerInstance.create));
 
-router.route("/:id")
-    .get(asyncHandler(userControllerInstance.findById))
+router.route("/:id").get(asyncHandler(userControllerInstance.findById));
 
 export default router;
